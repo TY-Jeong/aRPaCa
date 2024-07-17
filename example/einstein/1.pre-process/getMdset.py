adb5fc7522c92c569e891e6e320ce25daa26f7dc
cmd_jty <helian97@snu.ac.kr>
2024-07-17 11:14:26 +0900

    cmd

---
+++ example/einstein/1.pre-process/getMdset.py
@@ -0,0 +1,7 @@
+import sys
+import numpy as np
+sys.path.append('/home/taeyoung/github/aRPaCa/arpaca/einstein')
+from file_manager import *
+
+getMDset(path_poscar='ensembles',
+         temp=np.arange(1200, 1900+1, 100))
---
+++ example/einstein/2.gather_results/getMDresult.py
@@ -0,0 +1,6 @@
+import sys
+import numpy as np
+sys.path.append('/home/taeyoung/github/aRPaCa/arpaca/einstein')
+from file_manager import *
+
+getMDresult()

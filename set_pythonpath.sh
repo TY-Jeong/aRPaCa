7a50fdd98cc5079f3fc3658f4dad460fa8cf5368
cmd_jty <helian97@snu.ac.kr>
2024-07-17 14:27:56 +0900

    cmd

--- set_pythonpath.sh
+++ set_pythonpath.sh
@@ -1,3 +1,5 @@
 path_now=`pwd`
-echo "export PYTHONPATH=${path_now}" >> ~/.bashrc # for linux or mac
+echo " " >> ~/.zshrc
+echo "##### For aRPaCa" >> ~/.zshrc
+echo "export PYTHONPATH=${path_now}" >> ~/.zshrc # for linux or mac
 #set PYTHONPATH=${path_now} # for window

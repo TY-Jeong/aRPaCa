6e152c9e225734892247db16f654f5bac9b75c08
정태영 <helianthus312@gmail.com>
2024-07-21 08:26:43 +0900

    cmd

--- set_pythonpath.sh
+++ set_pythonpath.sh
@@ -3,12 +3,12 @@ path_now=`pwd`
 # for bash user
 # echo " " >> ~/.bashrc
 # echo "##### For aRPaCa" >> ~/.bashrc
-# echo "export PYTHONPATH=${path_now}" >> ~/.bashrc
+# echo "export PYTHONPATH=${path_now}:$PYTHONPATH" >> ~/.bashrc
 
 # for zsh user
 echo " " >> ~/.zshrc
 echo "##### For aRPaCa" >> ~/.zshrc
-echo "export PYTHONPATH=${path_now}" >> ~/.zshrc
+echo "export PYTHONPATH=${path_now}:$PYTHONPATH" >> ~/.zshrc
 
 # for window
 # set PYTHONPATH=${path_now} # for window
